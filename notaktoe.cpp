54eedfc78f8747434166cd76fe32e381b3e05e0e
David Crandall <djcran@indiana.edu>
2018-07-10 18:53:41 -0400

    adding hybid cache version

--- notaktoe.cpp
+++ notaktoe.cpp
@@ -17,7 +17,7 @@ using namespace std;
 uint BOARD_ROWS, BOARD_COLS, BOARD_SIZE;
 state_type WIN_MASK_HORIZ, WIN_MASK_VERT, WIN_MASK_DIAG2, WIN_MASK_DIAG1;
 bool p1(state_type, int);
-typedef enum {BITCACHE=0, HASHCACHE=1, BOTTOMUP=2} Algos;
+typedef enum {BITCACHE=0, HASHCACHE=1, BOTTOMUP=2, HYBRID=3} Algos;
 Algos  ALGO;
 // set disable_diag to true if you don't want to count complete diagonals as terminal states
 bool DISABLE_DIAG=false;
@@ -78,176 +78,222 @@ bool check_win(state_type state)
 // simple bit cache -- see if we already know value for this state, if so return it,
 //  if not compute and return it
 //
-state_type TOP_BITS=2, TOP_BITS_MASK=3, NOTOPBITS_MASK;
+state_type TOP_BITS=11,  NOTOPBITS_MASK, TOP_BITS_MASK;
 state_type **computed_hash;
 state_type *CACHE_MASK;
 state_type MAX_BIT=0;
 unsigned int *CACHE_BITS;
-
-
+state_type HYBRID_MASK, HYBRID_BITS = 26;
+state_type *ch_hybrid;
 state_type *bitvalue_hash;
 void bitcache_init(state_type count)
 {
+  cerr << "initializing cache..." << endl;
   //    bitcomputed_hash = (char *) memset(new char[count/8+1], 0, count/8+1);
-  bitvalue_hash = (state_type *) memset(new state_type[count/4/8/(state_type)pow(2,TOP_BITS)+1], 0, (count/4/8/pow(2,TOP_BITS)+1) * sizeof(state_type));
-  NOTOPBITS_MASK = (~((TOP_BITS_MASK) << (BOARD_SIZE-TOP_BITS))) & (MAX_BIT-1);
-}
-
-//inline uint8_t ht_bitpattern(state_type state)  { return uint8_t(1) << (uint8_t(state) & uint8_t(7)); }
-//inline uint8_t ht_bitpattern(state_type state)  { return uint8_t(3) << ((uint8_t(state) & uint8_t(3)) << 1); }
-//inline state_type ht_index(state_type state) { return state >> state_type(5); }
-//inline state_type ht_index(state_type state) { return state >> state_type(3); }
-
-
-// Functions to quickly compute equivalent states (rotations, etc.) for a given state
-//
-/////
-// fast bit reversal code from: https://stackoverflow.com/questions/746171/most-efficient-algorithm-for-bit-reversal-from-msb-lsb-to-lsb-msb-in-c
-static const state_type BitReverseTable256[] = 
-  {
-    0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
-    0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
-    0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
-    0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
-    0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 
-    0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
-    0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 
-    0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
-    0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
-    0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 
-    0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
-    0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
-    0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 
-    0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
-    0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 
-    0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
-  };
-
-// fast bit rversal
-inline state_type bit_reverse8(state_type state, uint bits) {   return ((BitReverseTable256[(state) & 0xff])) >> (8-bits);  }
-inline state_type bit_reverse(state_type state, uint bits)
-{
-  return (           (BitReverseTable256[(state) & 0xff] << 56) | 
-	  (BitReverseTable256[(state>> 8) & 0xff] << 48) | 
-	  (BitReverseTable256[(state>> 16) & 0xff] << 40) |
-	  (BitReverseTable256[(state>> 24) & 0xff] << 32) |
-          (BitReverseTable256[(state>> 32) & 0xff] << 24) | 
-	  (BitReverseTable256[(state>> 40) & 0xff] << 16) | 
-	  (BitReverseTable256[(state>> 48) & 0xff] << 8) |
-	  (BitReverseTable256[(state>> 56) & 0xff] << 0) 
-    ) >> (64-bits);
-}
-////////////////
-
-// each value of type from 0 to 5 gives a different reflection, rotation, etc of the state.
-//
-state_type get_equiv_state(state_type state, int type)
-{
-  state_type s2 = 0, state2=state;
-
-  switch(type) 
+  if(ALGO==HYBRID)
     {
-    case 0:
-      return state;
-    case 1: // 180 deg rotation
-      return bit_reverse(state, BOARD_SIZE);
-    case 2: // vert flip
-      for(int i=0; i<BOARD_ROWS; i++)
-	{
-	  s2 = (s2 << BOARD_COLS) | (state2 & WIN_MASK_HORIZ);
-	  state2 >>= BOARD_COLS;
-	}
-      return s2;
-    case 3: // horiz flip
-      for(int i=0; i<BOARD_COLS; i++)
-	{
-	  s2 = (s2 << 1) | (state2 & WIN_MASK_VERT);
-	  state2 >>= 1;
-	}
-      return s2;
-    case 4: // transpose and 90 deg
-    case 5:
-      for(int i=0; i<BOARD_ROWS; i++)
-	{
-	  state_type s3=0;
-	  state_type row = state2 & WIN_MASK_HORIZ;
-	  
-	  state2 >>= BOARD_COLS;
-	  row = bit_reverse8(row, BOARD_COLS);
-	  
-	  for(int j=0; j<BOARD_COLS; j++)
-	    {
-	      s3 = (s3 << BOARD_COLS) | (row & 1);
-	      row >>= 1;
-	    }
-	  
-	  s2 = (type == 4) ? (s2 | (s3 << i)) : ((s2 << 1) | s3);
-	}
+      cerr << "caches have size " << sizeof(state_type) * (count/4/8/(state_type)pow(2,TOP_BITS)+1) << " bytes, " << (int)pow(2, HYBRID_BITS) * sizeof(state_type) << " bytes" << endl;
+      bitvalue_hash = (state_type *) memset(new state_type[count/4/8/(state_type)pow(2,TOP_BITS)+1], 0, (count/4/8/pow(2,TOP_BITS)+1) * sizeof(state_type));
+      TOP_BITS_MASK = state_type(pow(2, TOP_BITS)-1);
+      NOTOPBITS_MASK = (~((TOP_BITS_MASK) << (BOARD_SIZE-TOP_BITS))) & (MAX_BIT-1);
       
-      return s2;
+
+      HYBRID_MASK = state_type(pow(2, HYBRID_BITS) - 1);
+      ch_hybrid = (state_type *) memset(new state_type[(int)pow(2, HYBRID_BITS)], 0, sizeof(state_type) * (int)pow(2, HYBRID_BITS));
+      ch_hybrid[0] = -1;
     }
-  return state;
-}
+  else
+    bitvalue_hash = (state_type *) memset(new state_type[count/4/8+1], 0, (count/4/8+1) * sizeof(state_type));
+   cerr << "done" << endl;
+ }
 
-void cache_init(state_type count, int levels=1)
-{
-  cerr << "initializing " << endl;
-  computed_hash = new state_type *[levels];
-  CACHE_MASK = new state_type[levels];
-  CACHE_BITS = new unsigned int[levels];
-  
-  for(int i=0; i<levels; i++)
-    {
-      CACHE_BITS[i] = min(5*(i+1), 26);
-      CACHE_MASK[i] = state_type(pow(2, CACHE_BITS[i]) - 1);
-      computed_hash[i] = (state_type *) memset(new state_type[(int)pow(2, CACHE_BITS[i])], 0, sizeof(state_type) * (int)pow(2, CACHE_BITS[i]));
-      computed_hash[i][0] = -1;
-    }
-
-  cerr << "done initializing " << endl;
-}
+ //inline uint8_t ht_bitpattern(state_type state)  { return uint8_t(1) << (uint8_t(state) & uint8_t(7)); }
+ //inline uint8_t ht_bitpattern(state_type state)  { return uint8_t(3) << ((uint8_t(state) & uint8_t(3)) << 1); }
+ //inline state_type ht_index(state_type state) { return state >> state_type(5); }
+ //inline state_type ht_index(state_type state) { return state >> state_type(3); }
 
 
-inline bool cache_get_val(state_type state, int arg2)
-{
-  state_type *ch;
-  if(ALGO == HASHCACHE)
-      ch = computed_hash[arg2];
+ // Functions to quickly compute equivalent states (rotations, etc.) for a given state
+ //
+ /////
+ // fast bit reversal code from: https://stackoverflow.com/questions/746171/most-efficient-algorithm-for-bit-reversal-from-msb-lsb-to-lsb-msb-in-c
+ static const state_type BitReverseTable256[] = 
+   {
+     0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
+     0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
+     0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
+     0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
+     0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 
+     0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+     0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 
+     0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+     0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+     0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 
+     0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+     0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+     0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 
+     0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+     0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 
+     0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+   };
 
-  // look at equivalent states (optionally)
-  for(int i=0; i<=READ_EQUIV; i++)
-    {
-      state_type s2 = get_equiv_state(state, i);
-      if(ALGO == BITCACHE)
-	{
-	  if(((s2 >> (BOARD_SIZE-TOP_BITS)) == TOP_BITS_MASK))
-	    {
-	      state_type val = bitvalue_hash[ (s2 & NOTOPBITS_MASK) >> state_type(5) ] >> ((s2 & 31) << 1);
-		  //	  unsigned char val = (((unsigned char *)bitvalue_hash)[ state >> state_type(2) ] >> ((s2 & 3) << 1));
-	  if(val & 1)
-	    return val & 2;
-	    }
-	}
-      else 
-	{
-	  state_type ch_hash = ch[get_hash(s2) & CACHE_MASK[arg2-2]]; 
-	  if((ch_hash >> 8) == s2)
-	    return ch_hash & 1;
-	}
-    }
+ // fast bit rversal
+ inline state_type bit_reverse8(state_type state, uint bits) {   return ((BitReverseTable256[(state) & 0xff])) >> (8-bits);  }
+ inline state_type bit_reverse(state_type state, uint bits)
+ {
+   return (           (BitReverseTable256[(state) & 0xff] << 56) | 
+	   (BitReverseTable256[(state>> 8) & 0xff] << 48) | 
+	   (BitReverseTable256[(state>> 16) & 0xff] << 40) |
+	   (BitReverseTable256[(state>> 24) & 0xff] << 32) |
+	   (BitReverseTable256[(state>> 32) & 0xff] << 24) | 
+	   (BitReverseTable256[(state>> 40) & 0xff] << 16) | 
+	   (BitReverseTable256[(state>> 48) & 0xff] << 8) |
+	   (BitReverseTable256[(state>> 56) & 0xff] << 0) 
+     ) >> (64-bits);
+ }
+ ////////////////
 
-  // cache miss, so need to calculate
-  bool val = p1(state, arg2);
-  for(int i=0; i<=WRITE_EQUIV; i++)
-    {
-      state_type s2 = get_equiv_state(state, i);
-      if(ALGO == BITCACHE)
-	{
-	  if(((s2 >> (BOARD_SIZE-TOP_BITS)) == TOP_BITS_MASK))
-	  bitvalue_hash[ (s2 & NOTOPBITS_MASK) >> state_type(5) ] |= ((val?3L:1L) << ((uint8_t(s2) & uint8_t(31)) << 1));
-		//		cout << state_to_string(s2) << endl;
-	}
-      //	((unsigned char*)bitvalue_hash)[  state >> state_type(2) ] |= ((val?3:1) << ((uint8_t(s2) & uint8_t(3)) << 1));
+ // each value of type from 0 to 5 gives a different reflection, rotation, etc of the state.
+ //
+ state_type get_equiv_state(state_type state, int type)
+ {
+   state_type s2 = 0, state2=state;
+
+   switch(type) 
+     {
+     case 0:
+       return state;
+     case 1: // 180 deg rotation
+       return bit_reverse(state, BOARD_SIZE);
+     case 2: // vert flip
+       for(int i=0; i<BOARD_ROWS; i++)
+	 {
+	   s2 = (s2 << BOARD_COLS) | (state2 & WIN_MASK_HORIZ);
+	   state2 >>= BOARD_COLS;
+	 }
+       return s2;
+     case 3: // horiz flip
+       for(int i=0; i<BOARD_COLS; i++)
+	 {
+	   s2 = (s2 << 1) | (state2 & WIN_MASK_VERT);
+	   state2 >>= 1;
+	 }
+       return s2;
+     case 4: // transpose and 90 deg
+     case 5:
+       for(int i=0; i<BOARD_ROWS; i++)
+	 {
+	   state_type s3=0;
+	   state_type row = state2 & WIN_MASK_HORIZ;
+
+	   state2 >>= BOARD_COLS;
+	   row = bit_reverse8(row, BOARD_COLS);
+
+	   for(int j=0; j<BOARD_COLS; j++)
+	     {
+	       s3 = (s3 << BOARD_COLS) | (row & 1);
+	       row >>= 1;
+	     }
+
+	   s2 = (type == 4) ? (s2 | (s3 << i)) : ((s2 << 1) | s3);
+	 }
+
+       return s2;
+     }
+   return state;
+ }
+
+ void cache_init(state_type count, int levels=1)
+ {
+   cerr << "initializing " << endl;
+   computed_hash = new state_type *[levels];
+   CACHE_MASK = new state_type[levels];
+   CACHE_BITS = new unsigned int[levels];
+
+   for(int i=0; i<levels; i++)
+     {
+       CACHE_BITS[i] = min(5*(i+1), 26);
+       CACHE_MASK[i] = state_type(pow(2, CACHE_BITS[i]) - 1);
+       computed_hash[i] = (state_type *) memset(new state_type[(int)pow(2, CACHE_BITS[i])], 0, sizeof(state_type) * (int)pow(2, CACHE_BITS[i]));
+       computed_hash[i][0] = -1;
+     }
+
+   cerr << "done initializing " << endl;
+ }
+
+vector<state_type> ct(16);
+state_type ct2=0;
+
+state_type r_hybrid=0, w_hybrid=0;
+ inline bool cache_get_val(state_type state, int arg2)
+ {
+   state_type *ch;
+   if(ALGO == HASHCACHE)
+       ch = computed_hash[arg2];
+
+   // look at equivalent states (optionally)
+   for(int i=0; i<=READ_EQUIV; i++)
+     {
+       state_type s2 = get_equiv_state(state, i);
+       if(ALGO == HYBRID)
+	 {
+	   if(((s2 >> (BOARD_SIZE-TOP_BITS)) == TOP_BITS_MASK))
+	     {
+	       state_type val = bitvalue_hash[ (s2 & NOTOPBITS_MASK) >> state_type(5) ] >> ((s2 & 31) << 1);
+	       if(val & 1)
+		 return val & 2;
+	     }
+	   else
+	     {
+	       r_hybrid++;
+	       state_type ch_hash = ch_hybrid[get_hash(s2) & HYBRID_MASK];
+	       if((ch_hash >> 8) == s2)
+		 return ch_hash & 1;
+	     }
+	 }
+       else if(ALGO == BITCACHE)
+	 {
+	   state_type val = bitvalue_hash[ (s2) >> state_type(5) ] >> ((s2 & 31) << 1);
+	   if(val & 1)
+	     return val & 2;
+	 }
+       else 
+	 {
+	   state_type ch_hash = ch[get_hash(s2) & CACHE_MASK[arg2-2]]; 
+	   if((ch_hash >> 8) == s2)
+	     return ch_hash & 1;
+	 }
+     }
+
+   // cache miss, so need to calculate
+   bool val = p1(state, arg2);
+   for(int i=0; i<=WRITE_EQUIV; i++)
+     {
+       state_type s2 = get_equiv_state(state, i);
+       ct2++;
+       ct[( s2 >> (BOARD_SIZE-4))]++;
+       if(!(ct2 % 50000000))
+	 {
+	   cout << "-----" << endl;
+	   cout << ct2 << endl;
+	   for(int i=0; i<16; i++)
+	     cout << i << " " << ct[i] << endl;
+	   
+	   cerr << r_hybrid << " " << w_hybrid << endl;
+	 }
+       if(ALGO == HYBRID)
+	 {
+	   if(((s2 >> (BOARD_SIZE-TOP_BITS)) == TOP_BITS_MASK))
+	     bitvalue_hash[ (s2 & NOTOPBITS_MASK) >> state_type(5) ] |= ((val?3L:1L) << ((uint8_t(s2) & uint8_t(31)) << 1));
+	   else
+	     {
+	       //	       cout << std::bitset<25>(s2) << endl;
+	     ch_hybrid[get_hash(s2) & HYBRID_MASK] = (s2 << 8) | val;
+	       w_hybrid++;
+	     }
+	 }
+       else if(ALGO == BITCACHE)
+	 bitvalue_hash[ (s2) >> state_type(5) ] |= ((val?3L:1L) << ((uint8_t(s2) & uint8_t(31)) << 1));
       else
 	ch[ get_hash(s2) & CACHE_MASK[arg2-2] ] = (s2 << 8) | val;
     }
@@ -278,6 +324,7 @@ state_type binom(state_type n, state_type k)
   return binom(n-1,k-1) + binom(n-1,k);                                                                                                                                          
 }
 
+
 int main(int argc, char *argv[])
 {
     try
@@ -306,7 +353,7 @@ int main(int argc, char *argv[])
         BOARD_COLS = atoi(argv[ii+1]);
 	BOARD_SIZE = BOARD_ROWS * BOARD_COLS;
 	MAX_BIT = pow(2, BOARD_SIZE);
-	if(ALGO <0 || ALGO > 3)
+	if(ALGO <0 || ALGO > 4)
 	  throw string("invalid algo " + ALGO);
 
 	init_check_win();
@@ -361,13 +408,15 @@ int main(int argc, char *argv[])
 	    return 0;
 	  }
 	
-	if(ALGO == BITCACHE)
+	if(ALGO == BITCACHE || ALGO == HYBRID)
 	  bitcache_init(MAX_BIT);
 	else if(ALGO == HASHCACHE)
 	  cache_init(pow(2,BOARD_ROWS*BOARD_COLS), BOARD_ROWS*BOARD_COLS);
 	
         cout << "Player 1 " << (p1(0000,1) ? "can" : "cannot") << " force a win." << endl;
 	
+	
+
 	/*	vector<state_type> ct(16);
 	for(state_type i=0; i<MAX_BIT/32; i++)
 	  {
@@ -383,10 +432,11 @@ int main(int argc, char *argv[])
 	    //	    cout << endl;
 	    //    if(!f) ct2+=4;
 	  }
-
+	*/
 	for(int i=0; i<16; i++)
 	  cout << i << " " << ct[i] << endl;
-	*/
+	
+	cerr << r_hybrid << " " << w_hybrid << endl;
         return 0;
     }
 
